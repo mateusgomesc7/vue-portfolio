0c9cb4caf8afa32b72b5c320e17d07e2dc2260cf
Mateus Gomes <mateus.correia@assert.ifpb.edu.br>
2023-06-21 16:55:13 -0300

    remove nuxt default files

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      fixed
+      app
+    >
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      title: 'Mateus Gomes'
+    }
+  }
+}
+</script>
