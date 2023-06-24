cf6d4ce91956a83118a42a9b0047a065a20a1801
Mateus Gomes <mateusgomes.dev@gmail.com>
2023-06-24 11:27:59 -0300

    center vertically

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,20 @@
 <template>
   <v-row no-gutters justify="center" align="center">
     <v-col class="d-flex justify-center" cols="12" md="5">
-        <Presentation />
+      <Presentation />
     </v-col>
-    <v-col cols="12" md="7">
-      <v-card class="py-4 d-flex justify-center" elevation="0">
-      </v-card>
+    <v-col class="d-flex justify-center" cols="12" md="7">
+      <Cubo />
     </v-col>
   </v-row>
 </template>
 
 <script>
 import Presentation from '@/components/Presentation.vue';
+import Cubo from '@/components/Cubo.vue';
 
 export default {
     name: "IndexPage",
-    components: { Presentation }
+    components: { Presentation, Cubo }
 }
 </script>
