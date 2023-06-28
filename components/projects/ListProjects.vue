0afa83dd59b88b6c8ebdd9cd34989180d0b295ce
Mateus Gomes <mateusgomes.dev@gmail.com>
2023-06-28 17:10:43 -0300

    update search in search and projects components

--- components/projects/ListProjects.vue
+++ components/projects/ListProjects.vue
@@ -36,21 +36,11 @@
 <script>
 export default {
     name: "ListProjects",
-    data() {
-        return {
-            projects: [
-                { name: 'Project 1' },
-                { name: 'Project 2' },
-                { name: 'Project 3' },
-                { name: 'Project 4' },
-                { name: 'Project 5' },
-                { name: 'Project 6' },
-                { name: 'Project 7' },
-                { name: 'Project 8' },
-                { name: 'Project 9' },
-                { name: 'Project 10' },
-            ]
-        }
+    props: {
+        projects: {
+            type: Array,
+            default: () => [],
+        },
     },
     computed: {
         firstColumn() {
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,20 +1,42 @@
 <template>
   <v-row no-gutters justify="center">
     <v-col cols="12">
-      <Search />
+      <Search :technologies="technologies" @search="handleSearch" />
     </v-col>
     <v-col cols="12">
-      <ListProjects />
+      <ListProjects :projects="filteredProjects"/>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import technologies from '~/static/data/technologies.json'
+import projects from '~/static/data/projects.json'
 import Search from '@/components/projects/Search.vue';
 import ListProjects from '@/components/projects/ListProjects.vue';
 
 export default {
     name: "ProjectsPage",
     components: { Search, ListProjects },
+    data() {
+        return {
+            technologies: technologies.technologies,
+            projects: projects.projects,
+            filteredProjects: projects.projects,
+        }
+    },
+    methods: {
+      handleSearch(value) {
+        if (value.length === 0) {
+          this.filteredProjects = this.projects;
+        } else {
+          this.filteredProjects = this.projects.filter(project => {
+            return project.technologies.some(technologyId => {
+              return value.includes(technologyId)
+            })
+          })
+        }
+      },
+    },
 }
 </script>
