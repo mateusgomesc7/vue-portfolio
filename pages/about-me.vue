29d9baebccea69d3a105be337e403f32ef2e1e6f
Mateus Gomes <mateus.correia@assert.ifpb.edu.br>
2023-07-04 16:56:06 -0300

    create rows and cols in about me

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -1,9 +1,32 @@
 <template>
-    <v-row justify="center" align="center">
-      <v-col cols="12" sm="8" md="6">
-        <v-card class="logo py-4 d-flex justify-center">
-            <h1>{{ $t('pages.about_me.title') }}</h1>
-        </v-card>
+    <v-row no-gutters class="h-100" align="center">
+      <v-col cols="12">
+        <v-row justify="center" align="center">
+          <v-col
+            class="d-flex"
+            :class="$vuetify.breakpoint.mdAndDown ? 'justify-center' : 'justify-end'"
+            cols="12"
+            md="5"
+          >
+            About me
+          </v-col>
+          <v-col
+            class="webgl-col d-flex justify-center"
+            cols="12"
+            md="7"
+            style="height: 500px;"
+            :class="{'mobile-height': $vuetify.breakpoint.mobile}"
+          >
+            3d model
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="12">
+        <v-row>
+          <v-col>
+            Timelines
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </template>
