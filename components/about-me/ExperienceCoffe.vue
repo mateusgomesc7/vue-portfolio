2254729f1dc313df617e455b3e4a146025cc145b
Mateus Gomes <mateusgomes.dev@gmail.com>
2023-07-04 19:28:49 -0300

    create CardAboutMe ExperienceCoffe Timeline

---
+++ components/about-me/ExperienceCoffe.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-card
+        height="500"
+        width="100%"
+        color="secundary"
+        class="d-flex justify-center align-center"
+    >
+        3d model
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'ExperienceCoffee',
+}
+</script>
