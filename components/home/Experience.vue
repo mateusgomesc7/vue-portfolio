d951c23b3a17171e0f371edaf74e8057f6d51fb4
Mateus Gomes <mateusgomes.dev@gmail.com>
2023-07-05 16:48:49 -0300

    create ThreeJsMixin

--- components/home/Experience.vue
+++ components/home/Experience.vue
@@ -5,47 +5,31 @@
 </template>
 
 <script>
-import {
-    Scene, PerspectiveCamera, Mesh,
-    BoxGeometry, MeshStandardMaterial,
-    WebGLRenderer, AmbientLight, DirectionalLight,
-    Raycaster, Vector2,
-} from 'three'
-
+import { Mesh, BoxGeometry, MeshStandardMaterial, Raycaster, Vector2 } from 'three'
+import { ThreeJsMixin } from '@/mixins/ThreeJsMixin.js'
 
 export default {
+    mixins: [ThreeJsMixin],
     data () {
         return {
-            scene: undefined,
-            sizes: {
-                width: undefined,
-                height: undefined
-            },
-            camera: undefined,
-            box: undefined,
-            ambientLight: undefined,
-            directionalLight: undefined,
-            directionalLightHelper: undefined,
-            renderer: undefined,
-            controls: undefined,
-            raycaster: undefined,
+            box: null,
+            raycaster: null,
             mouse: new Vector2(-1,-1),
             currentIntersect: null, 
-            time: Date.now()
+            time: Date.now(),
+            animationFrameId: null,
         }
     },
     mounted() {
-        this.createScene()
-        this.setRenderer()
-        window.addEventListener('resize', this.handleResize);
+        this.createWorld()
         this.$refs.card.addEventListener('click', this.mouseClick)
         this.$refs.card.addEventListener('mousemove', this.mouseMove)
         this.tick()
     },
     beforeDestroy() {
-        window.removeEventListener('resize', this.handleResize);
         this.$refs.card.removeEventListener('click', this.mouseClick)
         this.$refs.card.removeEventListener('mousemove', this.mouseMove)
+        cancelAnimationFrame(this.animationFrameId)
     },
     methods: {
         mouseClick() {
@@ -63,60 +47,13 @@ export default {
             this.mouse.x = event.offsetX / this.sizes.width * 2 - 1
             this.mouse.y = - (event.offsetY / this.sizes.height) * 2 + 1
         },
-        createScene() {
-            this.scene = new Scene();
-
-            const cardElement = this.$refs.card;
-            this.sizes.width = cardElement.offsetWidth;
-            this.sizes.height = cardElement.offsetHeight;
-
-            this.camera = new PerspectiveCamera(
-                75,
-                this.sizes.width / this.sizes.height,
-                0.1,
-                1000
-            )
-            this.camera.position.set(0,0,1.8)
-            this.scene.add(this.camera)
-
-            this.ambientLight = new AmbientLight()
-            this.ambientLight.intensity = 0.5
-            this.scene.add(this.ambientLight)
-
-            this.directionalLight = new DirectionalLight()
-            this.directionalLight.position.set(-1, 1, 2)
-            this.scene.add(this.directionalLight)
-
+        createWorld() {
             this.box = new Mesh(
                 new BoxGeometry(1, 1, 1),
                 new MeshStandardMaterial({ color: 0x616161 })
             )
             this.scene.add(this.box)
-
             this.raycaster = new Raycaster()
-        },
-        setRenderer() {
-            this.renderer = new WebGLRenderer({
-                canvas: document.querySelector('canvas.webgl'),
-                alpha: true
-            })
-
-            this.renderer.setSize(this.sizes.width, this.sizes.height)
-            this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-            this.renderer.render(this.scene, this.camera)
-        },
-        handleResize() {
-            const cardElement = this.$refs.card;
-            this.sizes.width = cardElement.offsetWidth;
-            this.sizes.height = cardElement.offsetHeight;
-
-            // Update camera
-            this.camera.aspect = this.sizes.width / this.sizes.height
-            this.camera.updateProjectionMatrix()
-
-            // Update renderer
-            this.renderer.setSize(this.sizes.width, this.sizes.height)
-            this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
         },
         tick() {
             // Time
@@ -140,7 +77,7 @@ export default {
                 this.currentIntersect = intersects[0]
             }
             else
-            {                
+            {
                 this.currentIntersect = null
             }
 
@@ -148,7 +85,7 @@ export default {
             this.renderer.render(this.scene, this.camera)
 
             // Call tick again on the next frame
-            window.requestAnimationFrame(this.tick)
+            this.animationFrameId = window.requestAnimationFrame(this.tick)
         }
     }
 }
