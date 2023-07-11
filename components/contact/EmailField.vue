d5922978c8e72a332315baac7ae5c136ae9cfc54
Mateus Gomes <mateus.correia@assert.ifpb.edu.br>
2023-07-11 18:59:01 -0300

    test email copy in https

---
+++ components/contact/EmailField.vue
@@ -0,0 +1,69 @@
+<template>
+    <v-card-text class="d-flex align-center">
+      <div
+        class="mr-2 text-button"
+        :class="$vuetify.breakpoint.mobile ? 'text-overline' : 'text-button'"
+      >
+        {{ email }}
+      </div>
+        <v-tooltip
+            v-model="isShowTooltip"
+            top
+            :open-on-click="false"
+            :open-on-focus="false"
+            :open-on-hover="false"
+        >
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                    text
+                    outlined
+                    small
+                    v-bind="attrs"
+                    v-on="on"
+                    @click="copyEmail"
+                >
+                    <v-icon small>
+                    mdi-content-copy
+                    </v-icon>
+                </v-btn>
+            </template>
+            <span>{{ tooltipMessage }}</span>
+        </v-tooltip>
+    </v-card-text>
+</template>
+
+<script>
+export default {
+    name: "EmailField",
+    data() {
+        return {
+          email: "mateusgomes.dev@gmail.com",
+          tooltipMessage: "Copiar e-mail",
+          isShowTooltip: false,
+        }
+    },
+    methods: {
+      copyEmail() {
+        if (window.location.protocol === 'https:') {
+          navigator.clipboard.writeText(this.email)
+            .then(() => {
+                this.tooltipMessage = "Copiado!";
+            })
+            .catch((error) => {
+              console.error('Erro ao copiar texto para a área de transferência:', error);
+            });
+        } else {
+            this.tooltipMessage = "Não é possível copiar o e-mail em uma conexão não segura.";
+
+            this.isShowTooltip = true;
+            setTimeout(() => {
+                this.isShowTooltip = false;
+            }, 3000);
+        }
+      },
+      showTooltip() {
+        this.isShowTooltip = true;
+      }
+    }
+}
+</script>
--- pages/contact.vue
+++ pages/contact.vue
@@ -12,27 +12,7 @@
         Se você tiver alguma pergunta, oportunidade de trabalho ou simplesmente quiser bater um papo, sinta-se à vontade:
       </div>
     </v-card-text>
-    <v-card-text class="d-flex align-center">
-      <div
-        class="mr-2 text-button"
-        :class="$vuetify.breakpoint.mobile ? 'text-overline' : 'text-button'"
-      >
-        mateusgomes.dev@gmail.com
-      </div>
-      <v-btn
-        text
-        outlined
-        small
-      >
-        <v-icon
-          :left="$vuetify.breakpoint.mobile ? false : true"
-          :small="$vuetify.breakpoint.mobile ? true : false"
-        >
-          mdi-content-copy
-        </v-icon>
-        {{ $vuetify.breakpoint.mobile ? '' : 'copy' }}
-      </v-btn>
-    </v-card-text>
+    <EmailField />
     <v-card-actions class="pt-4 pb-8">
       <v-btn
           depressed
@@ -61,8 +41,10 @@
 </template>
   
 <script>
+import EmailField from '@/components/contact/EmailField.vue';
 
 export default {
     name: "ContactPage",
+    components: { EmailField },
 }
 </script>
