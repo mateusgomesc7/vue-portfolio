133fd1f629cc4fb1a83ebc833f0029240e0f499e
Mateus Gomes <mateus.correia@assert.ifpb.edu.br>
2023-07-12 19:07:23 -0300

    put model 3d in contact

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -2,27 +2,9 @@
   <v-row no-gutters>
     <v-col
       cols="12"
-      :class="$vuetify.breakpoint.smAndDown ? 'pb-10' : 'py-10'"
+      class="pb-10 d-flex justify-center"
     >
-      <v-row no-gutters justify="center" align="center">
-        <v-col
-          class="d-flex"
-          :class="$vuetify.breakpoint.smAndDown ? 'justify-center' : 'justify-end'"
-          cols="12"
-          md="6"
-        >
-          <CardAboutMe />
-        </v-col>
-        <v-col
-          class="d-flex justify-center"
-          cols="12"
-          md="6"
-          style="height: 500px;"
-          :class="{'mobile-height': $vuetify.breakpoint.mobile}"
-        >
-          <ExperienceCoffee />
-        </v-col>
-      </v-row>
+      <CardAboutMe />
     </v-col>
     <v-col cols="12">
       <Timeline />
@@ -33,11 +15,10 @@
 <script>
 import Timeline from '@/components/about-me/Timeline.vue';
 import CardAboutMe from '@/components/about-me/CardAboutMe.vue';
-import ExperienceCoffee from '@/components/about-me/ExperienceCoffe.vue';
 
 export default {
     name: "AboutMePage",
-    components: { CardAboutMe, ExperienceCoffee, Timeline }
+    components: { CardAboutMe, Timeline }
 }
 </script>
 
