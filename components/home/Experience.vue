dcc77cf010c702be7f1459041ac6c554e5b89e71
Mateus Gomes <mateus.correia@assert.ifpb.edu.br>
2023-08-31 16:39:48 -0300

    put 3d model

--- components/home/Experience.vue
+++ components/home/Experience.vue
@@ -5,8 +5,10 @@
 </template>
 
 <script>
-import { Mesh, BoxGeometry, MeshStandardMaterial, Raycaster, Vector2 } from 'three'
+import { Mesh, BoxGeometry, MeshStandardMaterial, Raycaster, Vector2, TextureLoader, SRGBColorSpace, MeshBasicMaterial } from 'three'
 import { ThreeJsMixin } from '@/mixins/ThreeJsMixin.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 export default {
     mixins: [ThreeJsMixin],
@@ -49,11 +51,75 @@ export default {
         },
         createWorld() {
             this.box = new Mesh(
-                new BoxGeometry(1, 1, 1),
+                new BoxGeometry(0.2, 0.2, 0.2),
                 new MeshStandardMaterial({ color: 0x616161 })
             )
+            this.box.position.set(0, -0.1, 1.7)
             this.scene.add(this.box)
             this.raycaster = new Raycaster()
+
+            /**
+             * Loaders
+             */
+            // Texture loader
+            const textureLoader = new TextureLoader()
+
+            // Draco loader
+            const dracoLoader = new DRACOLoader()
+            dracoLoader.setDecoderPath('draco/')
+
+            // GLTF loader
+            const gltfLoader = new GLTFLoader()
+            gltfLoader.setDRACOLoader(dracoLoader)
+
+            /**
+             * Textures
+             */
+            const bakedTexture = textureLoader.load('baked.jpg')
+            bakedTexture.flipY = false
+            bakedTexture.colorSpace = SRGBColorSpace
+
+            /**
+             * Materials
+             */
+            // Baked Material
+            const bakedMaterial = new MeshBasicMaterial({ map: bakedTexture })
+
+            // Fan light Material
+            const fanLightMaterial = new MeshBasicMaterial({ color: 0xffffff })
+
+            // Symbol light Material
+            const symbolLightMaterial = new MeshBasicMaterial({ color: 0xffffe5 })
+
+            /**
+             * Model 
+             */
+            gltfLoader.load(
+                'computer.glb',
+                (gltf) =>
+                {
+                    gltf.scene.traverse((child) =>
+                    {
+                        child.material = bakedMaterial
+                    })
+
+                    const alexaLightMesh = gltf.scene.children.find(child => child.name === 'alexaLight')
+                    const fanLightAMesh = gltf.scene.children.find(child => child.name === 'fanLightA')
+                    const fanLightBMesh = gltf.scene.children.find(child => child.name === 'fanLightB')
+                    const symbolLightBMesh = gltf.scene.children.find(child => child.name === 'symbolLight')
+
+                    alexaLightMesh.material = fanLightMaterial
+                    fanLightAMesh.material = fanLightMaterial
+                    fanLightBMesh.material = fanLightMaterial
+                    symbolLightBMesh.material = symbolLightMaterial
+
+
+                    gltf.scene.position.y = -1
+                    gltf.scene.position.x = 0.55
+                    // gltf.scene.rotation.set(Math.PI/25, -Math.PI/3.5, 0)
+                    this.scene.add(gltf.scene)
+                }
+            )
         },
         tick() {
             // Time
@@ -61,10 +127,13 @@ export default {
             const deltaTime = currentTime - this.time
             this.time = currentTime
 
+            // Update controls
+            this.controls.update()
+
             // Update objects
             this.box.rotation.y += 0.0004 * deltaTime
             this.box.rotation.x += 0.0001 * deltaTime
-            this.box.position.y = Math.sin(this.time * 0.001) * 0.3
+            // this.box.position.y = Math.sin(this.time * 0.001) * 0.1
             this.box.updateMatrixWorld()
 
             // raycaster
