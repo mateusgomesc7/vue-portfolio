99d7abbe2b6ec42eb10fc5b18ffea79a38864cc0
Mateus Gomes <mateusgomes.dev@gmail.com>
2023-09-21 18:05:41 -0300

    put loading

---
+++ store/setup_3d/state.js
@@ -0,0 +1,9 @@
+export default () => ({
+    is_loaded: false,
+    materials: {
+        bakedLightMaterial: null,
+        bakedDarkMaterial: null,
+        fanLightMaterial: null,
+        symbolLightMaterial: null,
+    }
+})
